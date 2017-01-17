b5d9ec46490725a1df25c01cfec0fe880c552f6c
FuckingNoO <1158656977@qq.com>
2017-01-17 14:16:16 +0800

    custom.js fix

--- server/public/javascripts/dataplatform/custom.js
+++ server/public/javascripts/dataplatform/custom.js
@@ -1,8 +1,8 @@
-function getContent(evt, contentType) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("content");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+function getContent(event, contentType) {
+    var content = document.getElementsByClassName("content");
+    for (var i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
     }
     document.getElementById(contentType).style.display = "block";
+    document.getElementById('subdir-name').innerHTML = event.target.innerHTML;
 }
