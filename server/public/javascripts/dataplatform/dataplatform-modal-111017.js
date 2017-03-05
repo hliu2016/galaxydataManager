464cfff3a266c9a60e42745cfe53e2bfd7ef7b2f
FuckingNoO <1158656977@qq.com>
2017-03-05 15:52:03 +0800

    fix some error frontend

--- server/public/javascripts/dataplatform/dataplatform-modal-111017.js
+++ server/public/javascripts/dataplatform/dataplatform-modal-111017.js
@@ -1,12 +1,18 @@
 $(function () {
-    $("#disease-add-type-btn").click(function (event) {
+    $(".disease-add-type-btn").click(function (event) {
         event.preventDefault();
         var url = "/diseasetypeadd",
             dataArray = {
             diseaseType: $("#diseasetype").val()
         };
-        $.post(url, dataArray,function (data) {
-            alert(data);
+        $.post(url, dataArray,function (rs) {
+            if(rs.stateCode == 200) {
+                var html = '<li><input type="checkbox" value="0"/><span>'+rs.type+'</span></li>';
+                $(".disease-type ul").append(html);
+                alert('添加成功！')
+            }else{
+                alert('操作失败，稍后再试！');
+            }
         })
     })
 })
--- server/routes/diseaseTypeAdd/add.js
+++ server/routes/diseaseTypeAdd/add.js
@@ -1,11 +1,23 @@
-import * as model from '../../model/diseaseInfo/diseaseType'
+// import * as model from '../../model/diseaseInfo/diseaseType' todo
 import fs from 'fs'
-
+import Promise from 'bluebird'
 module.exports = async (req, res, next) => {
+    Promise.promisifyAll(fs)
     const diseaseInfo = { diseaseType: req.body.diseaseType }
     try {
-
-    }catch (e){
-        console.log(e||e.stack)
+        let dirpath = '/Users/fuckingnoob/Documents/WitSeq/' + diseaseInfo.diseaseType,
+            mode = '0777';
+        //todo 文件权限的设置
+        await fs.mkdir(dirpath,mode)
+        return res.json({
+            stateCode: '200',
+            type: diseaseInfo.diseaseType
+        })
+    }catch (err){
+        console.log(err||err.stack)
+        return res.json({
+            stateCode: '201',
+            msg: 'mkdir failed'
+        })
     }
 }
